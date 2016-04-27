32d01c77092e34c357613cb0b7ff7c79f43ce92b
genialx <admin@ihuxu.com>
2016-04-27 15:20:12 +0800

    remove comment

--- src/com/ihuxu/chatxserver/controller/Bootstrap.java
+++ src/com/ihuxu/chatxserver/controller/Bootstrap.java
@@ -6,21 +6,7 @@ import com.ihuxu.chatxserver.util.server.Server;
 
 public class Bootstrap {
 	public static void main(String []args) {
-		/**
-		
-		TextMessage textMessage = new TextMessage();
-		textMessage.set("name", "genialx");
-		textMessage.set("uid", "23392282");
-		MessageManager messageManager = new MessageManager(MessageManager.TYPE_CHAT_TEXT_MSG);
-		messageManager.setTextMessage(textMessage);
-		
-		if(messageManager.hasTextMessage()) {
-			System.out.println(messageManager.getTextMessage());
-		}
-		
-		**/
 		Server server = new Server();
 		server.start();
-		
 	}
 }
